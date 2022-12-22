22a006ed5e9104781da22a93bef165a7f22af521
Meepek666 <107631375+Meepek666@users.noreply.github.com>
2022-12-22 17:36:45 +0100

    index

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 let firstNumber = 2
 let secondNumber = 4
-suma = 2 + 4
-console.log(suma)
+num1=2
+num2=4
+function suma (num1,num2) {
+     let result = num1 + num2;
+
+     return result;
+}
+console.log( suma(2, 4) );
